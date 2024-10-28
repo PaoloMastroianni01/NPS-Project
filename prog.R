804d1b3f3c1f54f2518752ebfdaa2c600f83d53d
Paolo Mastroianni <paolomastroianni01@gmail.com>
2024-10-28 10:20:42 +0100

    Rimuovo cartella aggiuntiva

--- prog.R
+++ prog.R
@@ -1,155 +1,137 @@
 #LIBRARIES ------------
-library('readxl')
+library(readxl)
 library(DepthProc)
 library(aplpack)
+library(ggplot2)
+library(RColorBrewer)
+library(dplyr)
 
-#PREPROCESSING -----------
-#load data
-data_scientist <- read.csv('survey_results_public.csv',header=T)
+#read file
+data_scientist <- read.csv("reduced_dataset.csv",header=T)
 
-#countries
-countries <- c(
-  "Albania",
-  "Andorra",
-  "Armenia",
-  "Austria",
-  "Azerbaijan",
-  "Belarus",
-  "Belgium",
-  "Bosnia and Herzegovina",
-  "Bulgaria",
-  "Croatia",
-  "Cyprus",
-  "Czech Republic",
-  "Denmark",
-  "Estonia",
-  "Finland",
-  "France",
-  "Georgia",
-  "Germany",
-  "Greece",
-  "Hungary",
-  "Iceland",
-  "Ireland",
-  "Italy",
-  "Kazakhstan",
-  "Latvia",
-  "Liechtenstein",
-  "Lithuania",
-  "Luxembourg",
-  "Malta",
-  "Moldova",
-  "Monaco",
-  "Montenegro",
-  "Netherlands",
-  "North Macedonia",
-  "Norway",
-  "Poland",
-  "Portugal",
-  "Romania",
-  "Russia",
-  "San Marino",
-  "Serbia",
-  "Slovakia",
-  "Slovenia",
-  "Spain",
-  "Sweden",
-  "Switzerland",
-  "Turkey",
-  "Ukraine",
-  "United Kingdom of Great Britain and Northern Ireland",
-  "Vatican City",
-  'United States of America',
-  'Canada'
-)
+#PREPROCESSING -----------------------
+#transform in factor variables
+data_scientist[1:7] <- lapply(data_scientist[c(1:7)], as.factor)
+data_scientist[10:16] <- lapply(data_scientist[c(10:16)], as.factor)
+names(data_scientist)[names(data_scientist) == "C.."] <- "Cplusplus" #because ++ gives issues
 
-#keep just data scientists who work full-time in Europe or US/Canada
-data_scientist <- data_scientist[data_scientist$DevType %in% c('Data scientist or machine learning specialist') &
-                                   data_scientist$MainBranch %in% c('I am a developer by profession') & 
-                                   data_scientist$Employment %in% c('Employed, full-time') &
-                                   data_scientist$Country %in% countries, ]
+#remove people who are paid less than guys asking elemosina al semaforo
+data_scientist <- data_scientist[-which(data_scientist$ConvertedCompYearly<6000),]
+#remove Elon Musk
+data_scientist <- data_scientist[-which(data_scientist$ConvertedCompYearly>4000000),]
+#remove people who don't say their age
+data_scientist <- data_scientist[-which(data_scientist$Age=='Prefer not to say'),]
+#we will focus only on people who at least attended university
+data_scientist <- data_scientist[-which(data_scientist$EdLevel=='Associate degree (A.A., A.S., etc.)' |
+                                        data_scientist$EdLevel=='Primary/elementary school' |
+                                        data_scientist$EdLevel=='Secondary school (e.g. American high school, German Realschule or Gymnasium, etc.)'|
+                                        data_scientist$EdLevel=='Something else'),]
+#remove people who don't know the size of their company and those ones who are not in a company
+data_scientist <- data_scientist[-which(data_scientist$OrgSize=='I don’t know' |
+                                          data_scientist$OrgSize=='Just me - I am a freelancer, sole proprietor, etc.'),]
 
-#we add the corresponding number to each column to easily find the useless ones and remove them 
-#colnames(data_scientist) <- paste(colnames(data_scientist), seq_along(colnames(data_scientist)), sep = "_")
-#remove useless columns
-data_scientist <- data_scientist[,-c(1,2,3,5,7,9:14,17,18,23:64,67:83)]
-
-#count and remove the lines with NA
+#count NA for each variable
 na_count <- sapply(data_scientist, function(x) sum(is.na(x)))
 data.frame(NA_Count = na_count)
-#data_scientist_clean <- data_scientist[complete.cases(data_scientist$CompTotal_21), ]
+rm(na_count)
 
-#change in factor columns
-data_scientist[c(1,2,3,4,5,6,10)] <- lapply(data_scientist[c(1,2,3,4,5,6,10)], as.factor)
-str(data_scientist)
+#DATA VISUALIZATION ----------
+cols <- names(data_scientist)
+cols <- cols[-c(6,8:16)]
 
-# Passo 1: Contare le occorrenze di ciascuna nazione
-country_counts <- table(data_scientist$Country)
-# Passo 2: Identificare le nazioni con meno di 10 occorrenze
-countries_to_remove <- names(country_counts[country_counts < 10])
-# Passo 3: Rimuovere le osservazioni relative a quelle nazioni
-data_scientist_clean <- data_scientist[!(data_scientist$Country %in% countries_to_remove), ]
-
-# target languages
-target_languages <- c("C", "C#", "C++", "Julia", "MATLAB", "Python", "R", "Scala", "SQL")
-# Build empty auxiliary dataset in which for each observation we write 1 if it uses that language, no otherwise
-language_df <- as.data.frame(matrix(0, nrow = nrow(data_scientist), ncol = length(target_languages)))
-colnames(language_df) <- target_languages
-
-for (i in 1:nrow(data_scientist)) {
-  # Extract languages
-  languages_present <- unlist(strsplit(data_scientist$LanguageHaveWorkedWith[i], ";"))
-  # check presence o target languages
-  for (lang in target_languages) {
-    if (lang %in% languages_present) {
-      language_df[i, lang] <- 1
-    }
-  }
+#for each variable of interest, we plot it against the yearly compensation
+for(col in cols){
+attach(data_scientist)
+data_filtered <- data_scientist[!is.na(ConvertedCompYearly) & !is.na(col), ]
+detach(data_scientist)
+p <- ggplot(data_filtered, aes_string(x = col , y =' ConvertedCompYearly', fill = col)) +
+  geom_boxplot(outlier.color = "red", outlier.shape = 16, outlier.size = 2) +  # Aggiungi boxplot con outlier
+  scale_y_continuous(labels = scales::comma) +  # Formatta l'asse y con le virgole per i grandi numeri
+  labs(title = paste("Annual Salary for", col),
+       x = paste(col),
+       y = "Salario Annuale (USD)") +
+  theme_minimal() +
+  theme(
+    plot.title = element_text(hjust = 0.5, face = "bold", size = 16),
+    axis.text = element_text(size = 12),
+    axis.title = element_text(size = 14)
+  ) +
+  scale_fill_brewer(palette = "Set3")
+print(p)
 }
+rm(data_filtered)
+rm(cols)
+rm(col)
+rm(p)
+#what about italy
+#filter data
+data_europe <- data_scientist %>%
+  filter(!is.na(ConvertedCompYearly) & Country != "United States of America" & Country != "Canada" ) %>%
+  mutate(Region = ifelse(Country == "Italy", "Italy", "Other Europe"))
+data_america <- data_scientist %>%
+  filter(!is.na(ConvertedCompYearly) & Country == "United States of America" | Country == "Canada" | Country == "Italy" ) %>%
+  mutate(Region = ifelse(Country == "Italy", "Italy", "America"))
+data_experience <- data_scientist %>%
+  filter(!is.na(ConvertedCompYearly) & !is.na(WorkExp))
 
-#add auxiliary dataset to the original one and remove it
-data_scientist <- cbind(data_scientist, language_df)
-rm(language_df)
+#italy vs europe
+ggplot(data_europe, aes(x = Region, y = ConvertedCompYearly, fill = Region)) +
+  geom_boxplot(outlier.color = "red", outlier.shape = 16, outlier.size = 2) +
+  scale_y_continuous(labels = scales::comma) +
+  labs(title = "Annual Salary: Italy vs Other European Countries",
+       x = "Country",
+       y = "Annual Salary (USD)") +
+  theme_minimal() +
+  theme(
+    plot.title = element_text(hjust = 0.5, face = "bold", size = 16),
+    axis.text = element_text(size = 12),
+    axis.title = element_text(size = 14)
+  ) +
+  scale_fill_brewer(palette = "Set1")  # Palette di colori per differenziare i paesi
+rm(data_europe)
 
-#remove column with all used languages
-data_scientist <- data_scientist[,-c(7,8,9)]
+#italy vs US+Canada
+ggplot(data_america, aes(x = Country, y = ConvertedCompYearly, fill = Country)) +
+  geom_boxplot(outlier.color = "red", outlier.shape = 16, outlier.size = 2) +
+  scale_y_continuous(labels = scales::comma) +
+  labs(title = "Annual Salary: Italy vs United States and Canada",
+       x = "Country",
+       y = "Annual Salary (USD)") +
+  theme_minimal() +
+  theme(
+    plot.title = element_text(hjust = 0.5, face = "bold", size = 16),
+    axis.text = element_text(size = 12),
+    axis.title = element_text(size = 14)
+  ) +
+  scale_fill_brewer(palette = "Set2")  # Palette di colori per differenziare i paesi
+rm(data_america)
 
-write.csv(data_scientist,"reduced_dataset.csv",row.names = FALSE)
+#compensation over years of experience and in color put the categorical variable you want
+ggplot(data_experience, aes(x = WorkExp, y = ConvertedCompYearly)) +
+  geom_jitter(aes(color = Cplusplus), width = 0.2, alpha = 0.6) +  # Punti di esperienza
+  geom_smooth(method = "lm", se = FALSE, color = "darkred", linetype = "dashed", size = 1.2) +  # Linea di regressione
+  scale_y_continuous(labels = scales::comma) +
+  labs(title = "Annual Salary by Years of Experience",
+       x = "Years of Experience",
+       y = "Annual Salary (USD)") +
+  theme_minimal() +
+  theme(
+    plot.title = element_text(hjust = 0.5, face = "bold", size = 16),
+    axis.text = element_text(size = 12),
+    axis.title = element_text(size = 14)
+  ) +
+  scale_color_brewer(palette = "Set1") # Colori per evidenziare gli anni di esperienza
+rm(data_experience)
 
+#depth measures + bagplot
+data_filtered <- data_scientist %>% filter(!is.na(ConvertedCompYearly) & !is.na(WorkExp))
 
+depth_values <- depthContour(cbind(data_filtered$WorkExp, data_filtered$ConvertedCompYearly), 
+                             depth_params = list(method = "Tukey"))
 
-#DA QUI IN POI USARE IL REDUCED DATASET-------------------------------------------
-data_scientist <- read.csv("reduced_dataset.csv",header=T)
-data_scientist[1:6] <- lapply(data_scientist[c(1,2,3,4,5,6)], as.factor)
-
-#un po' di tabelle per vedere come sono bilanciate le varie classi
-table(data_scientist$Age)
-table(data_scientist$Country)
-table(data_scientist$RemoteWork)
-table(data_scientist$EdLevel)
-table(data_scientist$OrgSize)
-table(data_scientist$ICorPM)
-boxplot(data_scientist$ConvertedCompYearly)
-#rimuoviamo il fenomeno
-data_scientist <- data_scientist[-which(data_scientist$ConvertedCompYearly>4000000),]
-#plottiamo il salario in base alla fascia di età
-salaries <- na.omit(data_scientist$ConvertedCompYearly)
-age <- data_scientist$Age[which(!is.na(data_scientist$ConvertedCompYearly))]
-plot(age,salaries)
-#la mediana sembra crescere in funzione dell'età (sicuramente perchè aumentando l'età aumentano gli anni di esperienza)
-#notiamo altri possibili outlier (rimuoverli?)
-
-#e in Italia?
-
-boxplot(data_scientist$ConvertedCompYearly,data_scientist$ConvertedCompYearly[which(data_scientist$Country=="Italy")],names = c("Whole Data","Italy"),main="Salary",col="gold")
-
-#Ora consideriamo i developer che hanno indicato sia il salario che gli anni di esperienza
-data <- data_scientist[which(!is.na(data_scientist$ConvertedCompYearly) & !is.na(data_scientist$WorkExp)),]
-plot(data$WorkExp,data$ConvertedCompYearly)
-depthContour(cbind(data$WorkExp,data$ConvertedCompYearly),depth_params = list(method="Tukey"))
-depthPersp(cbind(data$WorkExp,data$ConvertedCompYearly),depth_params = list(method="Tukey"),plot_method = "rgl")
-
-bagplot_data <- bagplot(cbind(data$WorkExp,data$ConvertedCompYearly), factor = 3, show.whiskers = T)   #bagplot        
-bagplot_data
-outliers <- bagplot_data$pxy.outlier  #la funzione trova i punti fuori dal bagplot
+bagplot_data <- bagplot(cbind(data_filtered$WorkExp, data_filtered$ConvertedCompYearly), 
+                        factor = 3, show.whiskers = TRUE)
+outliers <- bagplot_data$pxy.outlier
 outliers
+
+#NOMPARAMETRIC INFERENCE --------------
---
+++ trashcan.R
@@ -0,0 +1,124 @@
+#PREPROCESSING FOR INITIAL DATASET--------
+data_scientist <- read.csv('survey_results_public.csv',header=T)
+
+#countries
+countries <- c(
+  "Albania",
+  "Andorra",
+  "Armenia",
+  "Austria",
+  "Azerbaijan",
+  "Belarus",
+  "Belgium",
+  "Bosnia and Herzegovina",
+  "Bulgaria",
+  "Croatia",
+  "Cyprus",
+  "Czech Republic",
+  "Denmark",
+  "Estonia",
+  "Finland",
+  "France",
+  "Georgia",
+  "Germany",
+  "Greece",
+  "Hungary",
+  "Iceland",
+  "Ireland",
+  "Italy",
+  "Kazakhstan",
+  "Latvia",
+  "Liechtenstein",
+  "Lithuania",
+  "Luxembourg",
+  "Malta",
+  "Moldova",
+  "Monaco",
+  "Montenegro",
+  "Netherlands",
+  "North Macedonia",
+  "Norway",
+  "Poland",
+  "Portugal",
+  "Romania",
+  "Russia",
+  "San Marino",
+  "Serbia",
+  "Slovakia",
+  "Slovenia",
+  "Spain",
+  "Sweden",
+  "Switzerland",
+  "Turkey",
+  "Ukraine",
+  "United Kingdom of Great Britain and Northern Ireland",
+  "Vatican City",
+  'United States of America',
+  'Canada'
+)
+
+#keep just data scientists who work full-time in Europe or US/Canada
+data_scientist <- data_scientist[data_scientist$DevType %in% c('Data scientist or machine learning specialist') &
+                                   data_scientist$MainBranch %in% c('I am a developer by profession') & 
+                                   data_scientist$Employment %in% c('Employed, full-time') &
+                                   data_scientist$Country %in% countries, ]
+
+#we add the corresponding number to each column to easily find the useless ones and remove them 
+#colnames(data_scientist) <- paste(colnames(data_scientist), seq_along(colnames(data_scientist)), sep = "_")
+#remove useless columns
+data_scientist <- data_scientist[,-c(1,2,3,5,7,9:14,17,18,23:64,67:83)]
+
+#count and remove the lines with NA
+na_count <- sapply(data_scientist, function(x) sum(is.na(x)))
+data.frame(NA_Count = na_count)
+#data_scientist_clean <- data_scientist[complete.cases(data_scientist$CompTotal_21), ]
+
+#change in factor columns
+data_scientist[c(1,2,3,4,5,6,10)] <- lapply(data_scientist[c(1,2,3,4,5,6,10)], as.factor)
+str(data_scientist)
+
+# Passo 1: Contare le occorrenze di ciascuna nazione
+country_counts <- table(data_scientist$Country)
+# Passo 2: Identificare le nazioni con meno di 10 occorrenze
+countries_to_remove <- names(country_counts[country_counts < 10])
+# Passo 3: Rimuovere le osservazioni relative a quelle nazioni
+data_scientist_clean <- data_scientist[!(data_scientist$Country %in% countries_to_remove), ]
+
+# target languages
+target_languages <- c("C++", "Julia", "MATLAB", "Python", "R", "Scala", "SQL")
+# Build empty auxiliary dataset in which for each observation we write 1 if it uses that language, no otherwise
+language_df <- as.data.frame(matrix(0, nrow = nrow(data_scientist), ncol = length(target_languages)))
+colnames(language_df) <- target_languaFOR BIG ges
+
+for (i in 1:nrow(data_scientist)) {
+  # Extract languages
+  languages_present <- unlist(strsplit(data_scientist$LanguageHaveWorkedWith[i], ";"))
+  # check presence o target languages
+  for (lang in target_languages) {
+    if (lang %in% languages_present) {
+      language_df[i, lang] <- 1
+    }
+  }
+}
+
+#add auxiliary dataset to the original one and remove it
+data_scientist <- cbind(data_scientist, language_df)
+rm(language_df)
+
+#remove column with all used languages
+data_scientist <- data_scientist[,-c(7,8,9)]
+#DATA VISUALIZATION ---------
+#plot histogram for each variable
+attach(data_scientist)
+for (col_name in names(data_scientist)) {
+  p <- ggplot(data_scientist, aes_string(x = col_name)) +
+    geom_bar(fill = "skyblue", color = "black") +
+    labs(title = paste("Occorrenze per", col_name),
+         x = col_name,
+         y = "Frequenza") +
+    theme_minimal() +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1))
+  
+  print(p)
+}
+detach(data_scientist)
