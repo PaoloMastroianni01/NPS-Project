6a1fb606cc8aec53b838bbe83e152c1559fcee2f
alby1923 <alby01.pecora@gmail.com>
2024-10-30 10:01:44 +0100

    Add files via upload

---
+++ DataVisualization.R
@@ -0,0 +1,167 @@
+library(readxl)
+library(DepthProc)
+library(aplpack)
+library(ggplot2)
+library(gridExtra)
+library(RColorBrewer)
+library(dplyr)
+
+#read file
+data_scientist <- read.csv("reduced_dataset.csv",header=T)
+
+#columns we want to plot
+cols <- names(data_scientist)
+cols <- cols[-c(6,8,9:16)]
+
+#for each variable of interest, we plot it against the yearly compensation
+for(col in cols){
+attach(data_scientist)
+data_filtered <- data_scientist[!is.na(ConvertedCompYearly) & !is.na(col), ]
+detach(data_scientist)
+p <- ggplot(data_filtered, aes_string(x = col , y =' ConvertedCompYearly', fill = col)) +
+  geom_boxplot(outlier.color = "red", outlier.shape = 16, outlier.size = 2) + # Aggiungi boxplot con outlier
+  scale_x_discrete(labels=NULL,drop = T) +
+  scale_y_continuous(labels = scales::comma) +  # Formatta l'asse y con le virgole per i grandi numeri
+  labs(title = paste("Annual Salary for", col),
+       x = paste(col),
+       y = "Annual Salary (USD)") +
+  theme_minimal() +
+  theme(
+    plot.title = element_text(hjust = 0.5, face = "bold", size = 16),
+    axis.text = element_text(size = 12),
+    axis.title = element_text(size = 14)
+  ) +
+  scale_fill_brewer(palette = "Set3")
+print(p)
+}
+rm(data_filtered)
+rm(cols)
+rm(col)
+rm(p)
+
+#histograms for each programming language (occurencies and boxplot for annual salary)
+cols <- names(data_scientist)
+cols <- cols[c(10:16)]
+
+
+attach(data_scientist)
+for (col_name in cols) {
+  data_filtered <- data_scientist[!is.na(ConvertedCompYearly) & !is.na(col), ]
+  #histogram occurencies (consider also observations who didnt say their salary)
+  p1 <- ggplot(data_scientist, aes_string(x = col_name)) +
+    geom_bar(fill = "skyblue", color = "black") +
+    labs(title = paste("Occurrencies for", col_name),
+         x = col_name,
+         y = "Frequencies") +
+    theme_minimal() +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1))
+  
+  # Filter data
+  data_0 <- data_filtered[data_filtered[[col_name]] == 0, ]
+  data_1 <- data_filtered[data_filtered[[col_name]] == 1, ]
+  
+  # Boxplot per chi ha col_name = 0
+  p2a <- ggplot(data_0, aes(y = ConvertedCompYearly)) +
+    geom_boxplot(fill = "red", color = "black") +
+    labs(title = paste("Annual Salary for", col_name, "NO"),
+         x = col_name,
+         y = "Annual Salary (USD)") +
+    theme_minimal()
+  
+  # Boxplot per chi ha col_name = 1
+  p2b <- ggplot(data_1, aes(y = ConvertedCompYearly)) +
+    geom_boxplot(fill = "lightgreen", color = "black") +
+    labs(title = paste("Annual Salary for", col_name, "YES"),
+         x = col_name,
+         y = "Annual Salary (USD)") +
+    theme_minimal()
+  
+  # Combina i due grafici in un layout a due colonne
+  grid.arrange(p1, p2a, p2b, ncol = 3)
+}
+detach(data_scientist)
+
+rm(col)
+rm(col_name)
+rm(cols)
+rm(p)
+rm(p1)
+rm(p2)
+rm(p2a)
+rm(p2b)
+rm(data_0)
+rm(data_1)
+rm(data_filtered)
+
+#what about italy
+#filter data
+data_europe <- data_scientist %>%
+  filter(!is.na(ConvertedCompYearly) & Country != "United States of America" & Country != "Canada" ) %>%
+  mutate(Region = ifelse(Country == "Italy", "Italy", "Other Europe"))
+data_america <- data_scientist %>%
+  filter(!is.na(ConvertedCompYearly) & Country == "United States of America" | Country == "Canada" | Country == "Italy" ) %>%
+  mutate(Region = ifelse(Country == "Italy", "Italy", "America"))
+data_experience <- data_scientist %>%
+  filter(!is.na(ConvertedCompYearly) & !is.na(WorkExp))
+
+#italy vs europe
+ggplot(data_europe, aes(x = Region, y = ConvertedCompYearly, fill = Region)) +
+  geom_boxplot(outlier.color = "red", outlier.shape = 16, outlier.size = 2) +
+  scale_y_continuous(labels = scales::comma) +
+  labs(title = "Annual Salary: Italy vs Other European Countries",
+       x = "Country",
+       y = "Annual Salary (USD)") +
+  theme_minimal() +
+  theme(
+    plot.title = element_text(hjust = 0.5, face = "bold", size = 16),
+    axis.text = element_text(size = 12),
+    axis.title = element_text(size = 14)
+  ) +
+  scale_fill_brewer(palette = "Set1")  # Palette di colori per differenziare i paesi
+rm(data_europe)
+
+#italy vs US+Canada
+ggplot(data_america, aes(x = Country, y = ConvertedCompYearly, fill = Country)) +
+  geom_boxplot(outlier.color = "red", outlier.shape = 16, outlier.size = 2) +
+  scale_y_continuous(labels = scales::comma) +
+  labs(title = "Annual Salary: Italy vs United States and Canada",
+       x = "Country",
+       y = "Annual Salary (USD)") +
+  theme_minimal() +
+  theme(
+    plot.title = element_text(hjust = 0.5, face = "bold", size = 16),
+    axis.text = element_text(size = 12),
+    axis.title = element_text(size = 14)
+  ) +
+  scale_fill_brewer(palette = "Set2")  # Palette di colori per differenziare i paesi
+rm(data_america)
+
+#compensation over years of experience and in color put the categorical variable you want
+ggplot(data_experience, aes(x = WorkExp, y = ConvertedCompYearly)) +
+  geom_jitter(aes(color = Age), width = 0.2, alpha = 0.6) +  # Punti di esperienza
+  geom_smooth(method = "lm", se = FALSE, color = "darkred", linetype = "dashed", size = 1.2) +  # Linea di regressione
+  #geom_hline(yintercept = median(data_scientist$ConvertedCompYearly,na.rm=T),color = "forestgreen", size=1.2) +
+  scale_y_continuous(labels = scales::comma) +
+  labs(title = "Annual Salary by Years of Experience",
+       x = "Years of Experience",
+       y = "Annual Salary (USD)") +
+  theme_minimal() +
+  theme(
+    plot.title = element_text(hjust = 0.5, face = "bold", size = 16),
+    axis.text = element_text(size = 12),
+    axis.title = element_text(size = 14)
+  ) +
+  scale_color_brewer(palette = "Set1") # Colori per evidenziare gli anni di esperienza
+rm(data_experience)
+
+
+#depth measures + bagplot
+data_filtered <- data_scientist %>% filter(!is.na(ConvertedCompYearly) & !is.na(WorkExp))
+
+depth_values <- depthContour(cbind(data_filtered$WorkExp, data_filtered$ConvertedCompYearly), 
+                             depth_params = list(method = "Tukey"))
+
+bagplot_data <- bagplot(cbind(data_filtered$WorkExp, data_filtered$ConvertedCompYearly), 
+                        factor = 3, show.whiskers = TRUE)
+outliers <- bagplot_data$pxy.outlier
+outliers
---
+++ Preprocessing.R
@@ -0,0 +1,143 @@
+#load data
+data_scientist <- read.csv('survey_results_public.csv',header=T)
+
+#countries we will focus on (Europe+US+Canada)
+countries <- c(
+  "Albania",
+  "Andorra",
+  "Armenia",
+  "Austria",
+  "Azerbaijan",
+  "Belarus",
+  "Belgium",
+  "Bosnia and Herzegovina",
+  "Bulgaria",
+  "Croatia",
+  "Cyprus",
+  "Czech Republic",
+  "Denmark",
+  "Estonia",
+  "Finland",
+  "France",
+  "Georgia",
+  "Germany",
+  "Greece",
+  "Hungary",
+  "Iceland",
+  "Ireland",
+  "Italy",
+  "Kazakhstan",
+  "Latvia",
+  "Liechtenstein",
+  "Lithuania",
+  "Luxembourg",
+  "Malta",
+  "Moldova",
+  "Monaco",
+  "Montenegro",
+  "Netherlands",
+  "North Macedonia",
+  "Norway",
+  "Poland",
+  "Portugal",
+  "Romania",
+  "Russia",
+  "San Marino",
+  "Serbia",
+  "Slovakia",
+  "Slovenia",
+  "Spain",
+  "Sweden",
+  "Switzerland",
+  "Turkey",
+  "Ukraine",
+  "United Kingdom of Great Britain and Northern Ireland",
+  "Vatican City",
+  'United States of America',
+  'Canada'
+)
+
+#keep just data scientists who work full-time in Europe or US/Canada
+data_scientist <- data_scientist[data_scientist$DevType %in% c('Data scientist or machine learning specialist') &
+                                   data_scientist$MainBranch %in% c('I am a developer by profession') & 
+                                   data_scientist$Employment %in% c('Employed, full-time') &
+                                   data_scientist$Country %in% countries, ]
+
+#we add the corresponding number to each column to easily find the useless ones and remove them 
+#colnames(data_scientist) <- paste(colnames(data_scientist), seq_along(colnames(data_scientist)), sep = "_")
+#remove useless columns
+data_scientist <- data_scientist[,-c(1,2,3,5,7,9:14,17,18,23:64,67:83)]
+
+#count and remove the lines with NA
+na_count <- sapply(data_scientist, function(x) sum(is.na(x)))
+data.frame(NA_Count = na_count)
+#data_scientist_clean <- data_scientist[complete.cases(data_scientist$CompTotal_21), ]
+
+#change in factor columns
+data_scientist[c(1,2,3,4,5,6,10)] <- lapply(data_scientist[c(1,2,3,4,5,6,10)], as.factor)
+str(data_scientist)
+
+# target languages
+target_languages <- c("C++", "Julia", "MATLAB", "Python", "R", "Scala", "SQL")
+# Build empty auxiliary dataset in which for each observation we write 1 if it uses that language, no otherwise
+language_df <- as.data.frame(matrix(0, nrow = nrow(data_scientist), ncol = length(target_languages)))
+colnames(language_df) <- target_languages
+
+for (i in 1:nrow(data_scientist)) {
+  # Extract languages
+  languages_present <- unlist(strsplit(data_scientist$LanguageHaveWorkedWith[i], ";"))
+  # check presence o target languages
+  for (lang in target_languages) {
+    if (lang %in% languages_present) {
+      language_df[i, lang] <- 1
+    }
+  }
+}
+
+#add auxiliary dataset to the original one and remove it
+data_scientist <- cbind(data_scientist, language_df)
+rm(language_df)
+
+#remove column with all used languages
+data_scientist <- data_scientist[,-c(7,8,9)]
+
+#transform in factor variables
+data_scientist[10:16] <- lapply(data_scientist[c(10:16)], as.factor)
+names(data_scientist)[names(data_scientist) == "C.."] <- "Cplusplus" #because ++ gives issues
+
+#remove people who have less than 1000 dollary per month salary
+data_scientist <- data_scientist[-which(data_scientist$ConvertedCompYearly<10000),]
+#remove non-sense salary
+data_scientist <- data_scientist[-which(data_scientist$ConvertedCompYearly>2000000),]
+#remove people who don't say their age
+data_scientist <- data_scientist[-which(data_scientist$Age=='Prefer not to say'),]
+#we will focus only on people who at least attended university
+data_scientist <- data_scientist[-which(data_scientist$EdLevel=='Associate degree (A.A., A.S., etc.)' |
+                                          data_scientist$EdLevel=='Primary/elementary school' |
+                                          data_scientist$EdLevel=='Secondary school (e.g. American high school, German Realschule or Gymnasium, etc.)'|
+                                          data_scientist$EdLevel=='Something else'),]
+#remove people who don't know the size of their company and those ones who are not in a company
+data_scientist <- data_scientist[-which(data_scientist$OrgSize=='I don’t know' |
+                                          data_scientist$OrgSize=='Just me - I am a freelancer, sole proprietor, etc.'),]
+
+#count NA for each variable
+na_count <- sapply(data_scientist, function(x) sum(is.na(x)))
+data.frame(NA_Count = na_count)
+rm(na_count)
+
+#create csv file with reduced dataset
+write.csv(data_scientist, "reduced_dataset.csv", row.names = FALSE)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
