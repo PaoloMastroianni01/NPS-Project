7cbe07d526e1c8f93acc55a42875c593d7611bca
alby1923 <alby01.pecora@gmail.com>
2024-11-30 14:36:58 +0100

    to do

--- Question2.R
+++ Question2.R
@@ -2,26 +2,36 @@ library(dplyr)
 library(ggplot2)
 set.seed(19)
 
-#anova + manova 
 data_scientist <- read.csv("reduced_dataset.csv",header=T)
 data_filtered <- data_scientist %>% filter(!is.na(ConvertedCompYearly))
 
-#REMOVE:
-#age -> X
 summary(aov(ConvertedCompYearly~EdLevel+OrgSize ,data = data_filtered))
+#WORKFLOW
+#COLUMNS TO REMOVE A PRIORI --------------
 
 #RemoteWork -> X
 #PurchInfluence -> X
+#age -> X
 
-#ANOVA 
-#EdLevel -> yes
-#OrgSize -> yes
-#IcorPM -> yes (TEST DI IPOTESI)
+#ASSESS SIGNIFICANCY OF CATEGORICAL VARIABLES WITH ANOVA ------------
 
-#REGRESSION WITH SIGNIFICANT VARIABLES
-#Country -> yes
-#workexp -> ok
-#compyearly -> response
+#SPLIT EUROPE AND AMERICA+CANADA!
 
-#splines con anni di esperienza 
-#
+#EdLevel
+#OrgSize
+#IcorPM (TEST DI IPOTESI)
+
+#REGRESSION WITH SIGNIFICANT VARIABLES USING LAB7+LAB8 (GAM) METHODS ---------------
+
+#Remove outliers? (using bagplot: income-work experience)
+
+#Country (random intercept?)
+#Work experience
+#Significant Categorical Variables 
+
+#find combination for GAM (look lab.8)
+
+#Comparison of Europe and America regression lines (no intercept)
+
+
+#QUANTILE REGRESSION -------------
