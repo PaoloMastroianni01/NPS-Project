425abc2d143b0ba6dbba2010b85edb20c39795ff
alby1923 <alby01.pecora@gmail.com>
2024-12-13 23:05:56 +0100

    PLEASE COME UP WITH AN IDEA

--- 7.CompensationPrediction.R
+++ 7.CompensationPrediction.R
@@ -1,84 +1,46 @@
-library(DepthProc)
-library(aplpack)
-library(ggplot2)
-library(gridExtra)
-library(RColorBrewer)
-library(dplyr)
-library(ISLR2)
-library(splines)
-library(KernSmooth)
-library(nlmeU)
-library(corrplot)
-library(nlme)
-library(lme4)
-library(insight)
-library(MuMIn)
-library(lattice)
 library(splines)
 library(conformalInference)
-set.seed(19)
 
-#LOAD AND CLEAN DATASETS -----------
-data_scientist <- read.csv("reduced_dataset.csv",header=T)
-data_filtered <- data_scientist %>% filter(!is.na(ConvertedCompYearly)& !is.na(WorkExp))
-data_filtered <- data_filtered[,c(3:8)]
+load('datasets_no_outliers.Rdata')
+data_europe <- datasets[["data_europe"]]
+data_america <- datasets[["data_america"]]
+data_full <- datasets[["data_full"]]
 
-data_america <- data_filtered %>%
-  filter((Country == "United States of America") & WorkExp <= 15 & EdLevel!="Some college/university study without earning a degree") %>%
-  mutate(Region = "America")
-data_america <- data_america[,-4]
+attach(data_full)
 
-depth_values <- depthContour(cbind(data_america$WorkExp, data_america$ConvertedCompYearly), 
-                             depth_params = list(method = "Tukey"),ylim = c(0,750000))
+#table(OrgSize, EdLevel)
 
-bagplot_data <- bagplot(cbind(data_america$WorkExp, data_america$ConvertedCompYearly), 
-                        factor = 3, show.whiskers = TRUE,ylim=c(0,750000))
-outliers <- bagplot_data$pxy.outlier
-coordinate <- cbind(data_america$WorkExp, data_america$ConvertedCompYearly)
-non_outliers_index <- !apply(coordinate, 1, function(row) any(row[1] == outliers[,1] & row[2] == outliers[,2]))
-data_america <- data_america[non_outliers_index, ]
+# Matrix of features of the observed data (solo WorkExp, senza i fattori)
+X_spline <- bs(WorkExp, degree = 2, df = 4)  # Spline su WorkExp
+X <- X_spline  # Solo spline su WorkExp
 
-attach(data_america)
-
-# Matrix of features of the observed data
-X_spline <- bs(WorkExp, degree=3, df=7)
-X_factors <- model.matrix(~ EdLevel + OrgSize - 1)
-X <- cbind(X_spline, X_factors)
 # Vector of responses of the observed dataset
 y <- ConvertedCompYearly
 
-WorkExp.grid <- seq(range(WorkExp)[1], range(WorkExp)[2], by = 1)
-# And evaluate the predictors over a grid
-X_test_spline <- matrix(bs(WorkExp.grid, degree=3, df=7), nrow=length(WorkExp.grid))
-# Ottieni i livelli unici delle variabili categoriche
-levels_EdLevel <- unique(EdLevel)
-levels_OrgSize <- unique(OrgSize)
+# Define grid for WorkExp and evaluate the spline basis
+WorkExp.grid <- seq(min(WorkExp), max(WorkExp), by = 1)
+X_test_spline <- bs(WorkExp.grid, degree = 2, df = 4)  # Spline per la griglia di WorkExp
 
-# Crea tutte le combinazioni possibili
-combinations <- expand.grid(EdLevel = levels_EdLevel, OrgSize = levels_OrgSize)
+# Funzioni di training e predizione per il Conformal Inference
+lm_train <- lm.funs(intercept = TRUE)$train.fun
+lm_predict <- lm.funs(intercept = TRUE)$predict.fun
 
-# Genera la model.matrix basata sulle combinazioni
-X_test_factors <- model.matrix(~ EdLevel + OrgSize - 1, data = combinations) 
-X_test_factors <- matrix(rep(t(X_test_factors), each = nrow(X_test_spline)), 
-                         ncol = ncol(X_test_factors), 
-                         byrow = FALSE)
-X_test_spline <- matrix(rep(as.vector(X_test_spline), 12), 
-                                 ncol = ncol(X_test_spline), 
-                                 byrow = FALSE)
-cat("Righe X_test_spline:", nrow(X_test_spline), "\n")
-cat("Righe X_test_factors:", nrow(X_test_factors), "\n")
-X_test_grid <- cbind(X_test_spline, X_test_factors)
-# Function to perform model training
-lm_train = lm.funs(intercept = T)$train.fun
+# Perform conformal prediction
+c_preds <- conformal.pred(x = X, y = y, x0 = X_test_spline,
+                          alpha = 0.1, verbose = FALSE, train.fun = lm_train,
+                          predict.fun = lm_predict, num.grid.pts = 200)
 
-# Function to perform point prediction
-lm_predict = lm.funs(intercept = T)$predict.fun
+# Estrai le predizioni e gli intervalli di confidenza
+predictions <- c_preds$pred
+upper_bounds <- c_preds$up
+lower_bounds <- c_preds$lo
 
-c_preds <- conformal.pred(x=X, y=y, x0=X_test_grid,
-                          alpha=0.1, verbose=F, train.fun = lm_train,
-                          predict.fun=lm_predict, num.grid.pts=200)
+# Visualizza i risultati
+plot(WorkExp, ConvertedCompYearly, xlim = range(WorkExp.grid), cex = 0.5, col = "lightblue",
+     main = 'Spline Regression with Conformal Prediction',
+     xlab = "Work Experience (Years)", ylab = "Converted Compensation Yearly")
+lines(WorkExp.grid, predictions, lwd = 2, col = "black", lty = 1)
+matlines(WorkExp.grid, cbind(upper_bounds, lower_bounds), lwd = 1, col = "black", lty = 2)
 
-plot(WorkExp, ConvertedCompYearly, xlim=range(WorkExp.grid), cex=.5, col ="lightblue",
-     main='Spline Regression')
-lines(WorkExp.grid, c_preds$pred, lwd=2, col="black", lty=1)
-matlines(WorkExp.grid, cbind(c_preds$up,c_preds$lo), lwd=1, col="black", lty=2)
+
+detach(data_full)
